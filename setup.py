5b736ed41fbeb5d62fcbf7f6cf8eb9f16c57882a
milochadam <miloch.adam@gmail.com>
2021-08-01 20:04:58 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
+from setuptools import find_packages
 from setuptools import setup
-setup()
+
+setup(
+    name='importsorcery',
+    version='0.1',
+    packages=find_packages(),
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': ['importsorcery=importsorcery:main'],
+    },
+)
