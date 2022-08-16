6fcff8d59e4f9bd77e804abb8d8d33c004b6eaeb
milochadam <miloch.adam@gmail.com>
2022-08-16 13:39:14 +0200

    cleanup arguments

--- importsorcery/main.py
+++ importsorcery/main.py
@@ -14,29 +14,24 @@ from importsorcery.index import Index
 
 class ImportSorceryArgs(Tap):
     symbol: str
-    index: Path
-    exclude: List[Path]
+    project_root: Path
+    exclude_dirs: List[Path]
     python_path: Path
     current_file: Optional[Path]
 
-    def configure(self) -> None:
-        self.add_argument('--python-path', '-p', type=Path)
-        self.add_argument('--current-file', type=Path)
-        self.add_argument('--exclude', '-e')
-
 
 def main(argv: Sequence[str] | None = None) -> int:
-    args = ImportSorceryArgs().parse_args()
+    args = ImportSorceryArgs(underscores_to_dashes=True).parse_args()
 
     index = Index()
     index.index_system_modules()
 
-    if args.index:
-        index.index_project(args.index)
+    if args.project_root:
+        index.index_project(args.project_root)
 
     if args.symbol:
         candidates = index.get_candidates(
-            project_root=args.index,
+            project_root=args.project_root,
             symbol=args.symbol,
             current_file_path=args.current_file,
         )
